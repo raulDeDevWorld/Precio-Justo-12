1cc3f6041af9b8c426e524e4835ffcae825a8585
raulDeDevWorld <71567510+raulDeDevWorld@users.noreply.github.com>
2023-05-31 13:50:32 -0400

    Update page.jsx
    Cc

--- src/app/Register/page.jsx
+++ src/app/Register/page.jsx
@@ -9,7 +9,7 @@ import Button from '../../components/Button'
 import Input from '@/components/Input'
 import Select from '@/components/Select'
 import { WithAuth } from '@/HOCs/WithAuth'
-
+  
   
 import { useRouter } from 'next/navigation';
 
